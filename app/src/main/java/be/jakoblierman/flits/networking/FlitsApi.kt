ebbfaca0b1ee82773825fbdee6c69a9b2a1a188e
Jakob Lierman <jakob.lierman@gmail.com>
2019-08-08 15:45:07 +0200

    Retrofit basics

---
+++ app/src/main/java/be/jakoblierman/flits/networking/FlitsApi.kt
@@ -0,0 +1,5 @@
+package be.jakoblierman.flits.networking
+
+interface FlitsApi {
+    
+}
---
+++ app/src/main/java/be/jakoblierman/flits/util/Constants.kt
@@ -0,0 +1,11 @@
+package be.jakoblierman.flits.util
+
+object Constants {
+
+    private const val PROJECT_ID = "flits-hogent"
+    const val BASE_URL = "https://firestore.googleapis.com/v1/projects/$PROJECT_ID/databases/(default)/documents/"
+
+    val NETWORK_TIMEOUT = 3000
+
+
+}
