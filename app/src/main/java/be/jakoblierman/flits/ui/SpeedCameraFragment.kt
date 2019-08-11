ab97ddbee45659c4539fe99b8b0ad7f3ac9a8eec
Jakob Lierman <jakob.lierman@gmail.com>
2019-08-12 00:38:58 +0200

    Speedcamera now correctly displaying in fragment

--- app/src/main/java/be/jakoblierman/flits/ui/SpeedCameraFragment.kt
+++ app/src/main/java/be/jakoblierman/flits/ui/SpeedCameraFragment.kt
@@ -6,25 +6,39 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import be.jakoblierman.flits.R
 import be.jakoblierman.flits.databinding.FragmentSpeedCameraBinding
 import be.jakoblierman.flits.viewmodels.SpeedCameraViewModel
+import kotlinx.android.synthetic.main.fragment_speed_camera.*
 
+private const val ARG_SPEEDCAMERA_ID = "speedCameraId"
 
 class SpeedCameraFragment : Fragment() {
 
+    private var speedCameraId: String? = null
+
     companion object {
-        fun newInstance() = SpeedCameraFragment()
+        @JvmStatic
+        fun newInstance(speedCameraId: String) =
+            SpeedCameraFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_SPEEDCAMERA_ID, speedCameraId)
+                }
+            }
     }
 
     private lateinit var viewModel: SpeedCameraViewModel
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            speedCameraId = it.getString(ARG_SPEEDCAMERA_ID)
+        }
+    }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         viewModel = ViewModelProviders.of(this).get(SpeedCameraViewModel::class.java)
 
         val binding: FragmentSpeedCameraBinding =
@@ -37,17 +51,20 @@ class SpeedCameraFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-/*
-        // Hides description elements if no description is present
-        if (viewModel.speedCamera.value!!.description.isBlank()) {
-            text_description_title.visibility = View.GONE
-            text_description_value.visibility = View.GONE
-        }
+        arguments?.getString("speedCameraId")?.let { viewModel.getSpeedCameraById(it) }
 
-        // Hides image is none is present
-        if (viewModel.speedCamera.value!!.imagePath.isBlank())
-            imageView.visibility = View.GONE
-*/
+        viewModel.speedCamera.removeObservers(this)
+        viewModel.speedCamera.observe(this, Observer { speedCamera ->
+            // Hides description elements if no description is present
+            if (speedCamera.description.isBlank()) {
+                text_description_title.visibility = View.GONE
+                text_description_value.visibility = View.GONE
+            }
+
+            // Hides image is none is present
+            if (speedCamera.imagePath.isBlank())
+                imageView.visibility = View.GONE
+        })
     }
 
 }
--- app/src/main/java/be/jakoblierman/flits/viewmodels/SpeedCameraViewModel.kt
+++ app/src/main/java/be/jakoblierman/flits/viewmodels/SpeedCameraViewModel.kt
@@ -1,16 +1,86 @@
 package be.jakoblierman.flits.viewmodels
 
-import androidx.lifecycle.ViewModel
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import androidx.lifecycle.MutableLiveData
+import be.jakoblierman.flits.api.FlitsApi
+import be.jakoblierman.flits.base.BaseViewModel
 import be.jakoblierman.flits.model.SpeedCamera
+import com.orhanobut.logger.Logger
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import javax.inject.Inject
 
-class SpeedCameraViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+class SpeedCameraViewModel : BaseViewModel() {
 
-    //private val _speedCamera = MutableLiveData<SpeedCamera>()
-    //
-    //val speedCamera: LiveData<SpeedCamera>
-    //    get() = _speedCamera
+    val speedCamera = MutableLiveData<SpeedCamera>()
+    val speedCameras = MutableLiveData<List<SpeedCamera>>()
+    val loadingVisibility = MutableLiveData<Int>()
+    val objectVisibility = MutableLiveData<Int>()
 
-    val speedCamera = SpeedCamera("location", "kind")
+    @Inject
+    lateinit var flitsApi: FlitsApi
+    private var disposables = CompositeDisposable()
+
+    init {
+        disposables.add(
+            flitsApi.getSpeedCameras()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { onRetrieveStart() }
+                .doOnTerminate { onRetrieveFinish() }
+                .subscribe(
+                    { results -> onRetrieveListSuccess(results) },
+                    { error -> onRetrieveError(error) }
+                )
+        )
+    }
+
+    private fun onRetrieveListSuccess(results: List<SpeedCamera>) {
+        speedCameras.value = results
+        Logger.i(results.toString())
+    }
+
+    fun getSpeedCameraById(id: String) {
+        disposables.add(
+            flitsApi.getSpeedCameraById(id)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .doOnSubscribe { onRetrieveStart() }
+                .doOnTerminate { onRetrieveFinish() }
+                .subscribe(
+                    { result -> onRetrieveSingleSuccess(result) },
+                    { error -> onRetrieveError(error) }
+                )
+        )
+    }
+
+    private fun onRetrieveError(error: Throwable) {
+        Logger.e(error.message!!)
+    }
+
+    private fun onRetrieveSingleSuccess(result: SpeedCamera) {
+        speedCamera.value = result
+        Logger.i(result.toString())
+    }
+
+    private fun onRetrieveFinish() {
+        loadingVisibility.value = GONE
+        objectVisibility.value = VISIBLE
+    }
+
+    private fun onRetrieveStart() {
+        loadingVisibility.value = VISIBLE
+        objectVisibility.value = GONE
+    }
+
+    /**
+     * Disposes the subscription when the [BaseViewModel] is no longer used.
+     */
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
+    }
 
 }
