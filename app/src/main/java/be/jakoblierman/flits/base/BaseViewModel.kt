fa3341503eaefa6c5749efe219400048024d92fd
Jakob Lierman <jakob.lierman@gmail.com>
2019-08-11 20:50:52 +0200

    Created BaseViewModel for use with Dagger

---
+++ app/src/main/java/be/jakoblierman/flits/base/BaseViewModel.kt
@@ -0,0 +1,34 @@
+package be.jakoblierman.flits.base
+
+import androidx.lifecycle.ViewModel
+import be.jakoblierman.flits.injection.component.DaggerViewModelInjectorComponent
+import be.jakoblierman.flits.injection.component.ViewModelInjectorComponent
+import be.jakoblierman.flits.injection.module.NetworkModule
+import be.jakoblierman.flits.viewmodels.AvgSpeedCheckViewModel
+import be.jakoblierman.flits.viewmodels.PoliceCheckViewModel
+import be.jakoblierman.flits.viewmodels.SpeedCameraViewModel
+
+abstract class BaseViewModel : ViewModel() {
+
+    private val injectorComponent: ViewModelInjectorComponent =
+        DaggerViewModelInjectorComponent
+            .builder()
+            .networkModule(NetworkModule)
+            .build()
+
+    init {
+        inject()
+    }
+
+    /**
+     * Injects the required dependencies
+     */
+    private fun inject() {
+        when (this) {
+            is SpeedCameraViewModel -> injectorComponent.inject(this)
+            is AvgSpeedCheckViewModel -> injectorComponent.inject(this)
+            is PoliceCheckViewModel -> injectorComponent.inject(this)
+        }
+    }
+
+}
