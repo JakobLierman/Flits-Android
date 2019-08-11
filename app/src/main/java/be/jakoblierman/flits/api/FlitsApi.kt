02750172c7a21d86934452786c39c31ae162aa2d
Jakob Lierman <jakob.lierman@gmail.com>
2019-08-12 01:34:28 +0200

    HTTP GET all items and item by id

--- app/src/main/java/be/jakoblierman/flits/api/FlitsApi.kt
+++ app/src/main/java/be/jakoblierman/flits/api/FlitsApi.kt
@@ -1,5 +1,7 @@
 package be.jakoblierman.flits.api
 
+import be.jakoblierman.flits.model.AvgSpeedCheck
+import be.jakoblierman.flits.model.PoliceCheck
 import be.jakoblierman.flits.model.SpeedCamera
 import io.reactivex.Observable
 import retrofit2.http.GET
@@ -18,4 +20,29 @@ interface FlitsApi {
      */
     @GET("speedCameras/{id}")
     fun getSpeedCameraById(@Path("id") id: String): Observable<SpeedCamera>
+
+    /**
+     * Get all AvgSpeedChecks
+     */
+    @GET("avgSpeedChecks")
+    fun getAvgSpeedChecks(): Observable<List<AvgSpeedCheck>>
+
+    /**
+     * Get AvgSpeedCheck by id
+     */
+    @GET("avgSpeedCheck/{id}")
+    fun getAvgSpeedCheckById(@Path("id") id: String): Observable<AvgSpeedCheck>
+
+    /**
+     * Get all PoliceChecks
+     */
+    @GET("policeChecks")
+    fun getPoliceChecks(): Observable<List<PoliceCheck>>
+
+    /**
+     * Get PoliceCheck by id
+     */
+    @GET("policeCheck/{id}")
+    fun getPoliceCheckById(@Path("id") id: String): Observable<PoliceCheck>
+    
 }
