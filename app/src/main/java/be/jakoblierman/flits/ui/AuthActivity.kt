2cc699e6c58f2d6261cd504f5f226a710fa05754
Jakob Lierman <jakob.lierman@gmail.com>
2019-08-15 17:53:06 +0200

    Created authentication layouts

---
+++ app/src/main/java/be/jakoblierman/flits/ui/AuthActivity.kt
@@ -0,0 +1,13 @@
+package be.jakoblierman.flits.ui
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import be.jakoblierman.flits.R
+
+class AuthActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_auth)
+    }
+}
